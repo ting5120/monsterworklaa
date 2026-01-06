120a43a69229a8e95fb1ca9f17888fe2bf05f074
ting5120 <a0970243798@gmail.com>
2026-01-06 16:45:30 +0800

    測試

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class 111 : MonoBehaviour
+public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
